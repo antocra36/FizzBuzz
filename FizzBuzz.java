83ea985dfc6b51ce28e8e01fbf06ac44a7a4373e
antocra36 <49763694+antocra36@users.noreply.github.com>
2020-07-21 21:32:12 -0400

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,20 @@
+package com.cognixia.jump.corejava;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num;
+      
+      for(num = 1; num<10; num++) {
+    	  
+    	  if (num%3!=0)System.out.println("Fizz"); 
+    	  
+    	 
+    		  
+    		  
+      }
+      
+	}
+
+}
